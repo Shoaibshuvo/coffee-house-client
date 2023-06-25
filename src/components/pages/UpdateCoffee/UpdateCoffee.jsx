107787c8cd6c50907436ab2ed6fabee1215d83c5
Shuvo <shoaibshuvo11@gmail.com>
2023-06-25 17:25:07 +0600

    first commit

---
+++ src/components/pages/UpdateCoffee/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h5>Update a Coffee</h5>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
